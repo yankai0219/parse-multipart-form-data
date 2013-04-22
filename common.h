7d735f06fcab882e9b2f94a666edaef1ba7b0053
yankai0219 <yankai0219@126.com>
2013-04-22 17:04:33 +0800

    Create common.h

---
+++ common.h
@@ -0,0 +1,16 @@
+#ifndef _COMMOM_H_
+#define _COMMON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <time.h>
+#define PRINT 1
+#endif
